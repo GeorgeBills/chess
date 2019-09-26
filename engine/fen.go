2875be53db7116b06b82673f7083b60b66e14301
George Bills <george.s.bills@gmail.com>
2019-09-26 22:58:22 +1000

    Implement parsing of FEN.

--- engine/fen.go
+++ engine/fen.go
@@ -1,6 +1,10 @@
 package engine
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
@@ -102,4 +106,224 @@ func (b Board) FEN() string {
 	sb.WriteString(strconv.Itoa(b.FullMoves()))
 
 	return sb.String()
+}
+
+// NewBoardFromFEN returns a new board initialised as per the provided
+// Forsyth–Edwards Notation. Only 8×8 boards are supported. No validation of
+// resulting board state is performed; e.g. if the FEN specifies multiple kings
+// per side then that's what the board will contain.
+func NewBoardFromFEN(fen io.Reader) (*Board, error) {
+	b := &Board{}
+	r := bufio.NewReader(fen)
+
+	unexpectingEOF := func(err error) error {
+		if err == io.EOF {
+			return io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	skipspace := func() error {
+		seen := false
+		for {
+			ch, err := r.ReadByte()
+			if err != nil {
+				return err
+			}
+			if ch != ' ' {
+				if !seen {
+					// require at least one space
+					return fmt.Errorf("unexpected '%c', expecting ' '", ch)
+				}
+				return r.UnreadByte()
+			}
+			seen = true
+		}
+	}
+
+	readuint8 := func() (uint8, error) {
+		var n uint8
+		seen := false
+		for {
+			ch, err := r.ReadByte()
+			if err == io.EOF && seen {
+				// expect at least one digit (which might be 0) before EOF
+				return n, err
+			}
+			if err != nil {
+				return 0, unexpectingEOF(err)
+			}
+			switch ch {
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				n = 10*n + uint8(ch-'0')
+				seen = true
+			case ' ':
+				err = r.UnreadByte()
+				return n, err
+			default:
+				return 0, fmt.Errorf("unexpected '%c', expecting [0-9]", ch)
+			}
+		}
+	}
+
+	// read the 64 squares first
+READ_SQUARES:
+	for i := 0; i < 64; {
+		ch, err := r.ReadByte()
+		if err != nil {
+			return nil, unexpectingEOF(err)
+		}
+
+		if i != 0 && i%8 == 0 {
+			// expect a / to indicate a new rank, which we immediately skip over
+			if ch != '/' {
+				return nil, fmt.Errorf("unexpected '%c', expecting '/'", ch)
+			}
+			ch, err = r.ReadByte()
+			if err != nil {
+				return nil, unexpectingEOF(err)
+			}
+		}
+
+		idx := i + 56 - 16*(i/8)
+
+		switch ch {
+		case 'P':
+			b.pawns |= 1 << idx
+			b.white |= 1 << idx
+		case 'N':
+			b.knights |= 1 << idx
+			b.white |= 1 << idx
+		case 'B':
+			b.bishops |= 1 << idx
+			b.white |= 1 << idx
+		case 'R':
+			b.rooks |= 1 << idx
+			b.white |= 1 << idx
+		case 'Q':
+			b.queens |= 1 << idx
+			b.white |= 1 << idx
+		case 'K':
+			b.kings |= 1 << idx
+			b.white |= 1 << idx
+		case 'p':
+			b.pawns |= 1 << idx
+			b.black |= 1 << idx
+		case 'n':
+			b.knights |= 1 << idx
+			b.black |= 1 << idx
+		case 'b':
+			b.bishops |= 1 << idx
+			b.black |= 1 << idx
+		case 'r':
+			b.rooks |= 1 << idx
+			b.black |= 1 << idx
+		case 'q':
+			b.queens |= 1 << idx
+			b.black |= 1 << idx
+		case 'k':
+			b.kings |= 1 << idx
+			b.black |= 1 << idx
+		case '1', '2', '3', '4', '5', '6', '7', '8':
+			i += int(ch - '0') // skip empty squares
+			continue READ_SQUARES
+		default:
+			return nil, fmt.Errorf("unexpected '%c', expecting [PNBRQKpnbrqk1-8]", ch)
+		}
+
+		i++
+	}
+
+	var err error
+
+	err = skipspace()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+
+	// read whose move it is (white or black)
+	tomove, err := r.ReadByte()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+	switch tomove {
+	case 'w':
+		b.meta |= White
+	case 'b':
+		b.meta |= Black
+	default:
+		return nil, fmt.Errorf("unexpected '%c', expecting [wb]", tomove)
+	}
+
+	err = skipspace()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+
+	// read castling information
+READ_CASTLING:
+	for {
+		ch, err := r.ReadByte()
+		if err != nil {
+			return nil, unexpectingEOF(err)
+		}
+		switch ch {
+		case 'K':
+			b.meta |= wcks
+		case 'Q':
+			b.meta |= wcqs
+		case 'k':
+			b.meta |= bcks
+		case 'q':
+			b.meta |= bcqs
+		case ' ':
+			err := r.UnreadByte()
+			if err != nil {
+				return nil, unexpectingEOF(err)
+			}
+			break READ_CASTLING
+		default:
+			return nil, fmt.Errorf("unexpected '%c', expecting [KQkq]", ch)
+		}
+	}
+
+	err = skipspace()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+
+	// read en passant square
+	ch, err := r.ReadByte()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+	if ch != '-' {
+		// TODO: support reading en passant square
+		return nil, errors.New("en passant unsupported")
+	}
+
+	err = skipspace()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+
+	// read number of half moves
+	b.half, err = readuint8()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+
+	err = skipspace()
+	if err != nil {
+		return nil, unexpectingEOF(err)
+	}
+
+	// read number of full moves
+	b.full, err = readuint8()
+	if err != nil && err != io.EOF {
+		// we're expecting an io.EOF err here
+		return nil, err
+	}
+
+	return b, nil
 }
--- engine/fen_test.go
+++ engine/fen_test.go
@@ -1,15 +1,60 @@
 package engine_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/GeorgeBills/chess/m/v2/engine"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFEN(t *testing.T) {
 	fen := engine.NewBoard().FEN()
 	expected := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 	assert.Equal(t, expected, fen)
+}
+
+func TestNewBoardFromFEN(t *testing.T) {
+	fen := strings.NewReader("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	expected := engine.NewBoard()
+	b, err := engine.NewBoardFromFEN(fen)
+	require.NoError(t, err)
+	assert.Equal(t, &expected, b)
+}
+
+func TestNewBoardFromInvalidFEN(t *testing.T) {
+	invalid := []struct{ fen, expected string }{
+		{"", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQ", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR ", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w ", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq ", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - ", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 ", "unexpected EOF"},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNRwKQkq-01", "unexpected 'w', expecting ' '"},                    // no whitespace
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1n", "unexpected 'n', expecting [0-9]"},            // invalid full moves char
+		{"rnbqkbnr/pppppppp/7/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "unexpected '/', expecting [PNBRQKpnbrqk1-8]"}, // empty n too low
+		{"rnbqkbnr/pppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "unexpected '/', expecting [PNBRQKpnbrqk1-8]"},     // rank char too short
+		{"pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "unexpected ' ', expecting '/'"},                        // missing rank
+		{"rnbqkbnx/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "unexpected 'x', expecting [PNBRQKpnbrqk1-8]"}, // invalid piece char
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR ? KQkq - 0 1", "unexpected '?', expecting [wb]"},              // invalid to move char
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w abcd - 0 1", "unexpected 'a', expecting [KQkq]"},            // invalid castling chars
+		// {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq z 0 1", ""}, // invalid en passant
+		// {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 999999", ""}, // number too large
+	}
+	for i, tt := range invalid {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			b, err := engine.NewBoardFromFEN(strings.NewReader(tt.fen))
+			assert.EqualError(t, err, tt.expected)
+			assert.Nil(t, b)
+		})
+	}
 }
