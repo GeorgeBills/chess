ee6e8de7936927dd56fc98eb2c857969f3901a8c
George Bills <george.s.bills@gmail.com>
2019-09-28 11:00:37 +1000

    Small debugging utility to print a uint64 as a board.

---
+++ cmd/printboard/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fatal := func(code int, v interface{}) {
+		fmt.Println(v)
+		os.Exit(code)
+	}
+
+	if len(os.Args) != 2 {
+		fatal(0, fmt.Sprintf("%s <board>", os.Args[0]))
+	}
+
+	bitstring := os.Args[1]
+
+	var board uint64
+	var err error
+	switch {
+	case strings.HasPrefix(bitstring, "0x"):
+		board, err = strconv.ParseUint(bitstring[2:], 16, 64)
+		if err != nil {
+			fatal(1, err)
+		}
+	case strings.HasPrefix(bitstring, "0b"):
+		board, err = strconv.ParseUint(bitstring[2:], 2, 64)
+		if err != nil {
+			fatal(1, err)
+		}
+	default:
+		fatal(0, fmt.Sprintf("invalid board: %s", bitstring))
+	}
+
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	for i := 0; i < 64; i++ {
+		if i != 0 && i%8 == 0 {
+			f.WriteRune('\n')
+		}
+		idx := i + 56 - 16*(i/8)
+		if board&(1<<idx) != 0 {
+			f.WriteRune('■')
+		} else {
+			f.WriteRune('□')
+		}
+	}
+}
