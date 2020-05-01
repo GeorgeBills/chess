5b60d6ad479678d426a7e1a84d424958249bebd3
George Bills <george.s.bills@gmail.com>
2020-05-01 20:47:59 +1000

    Initialise board from const FEN.

--- cmd/perft/main.go
+++ cmd/perft/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/GeorgeBills/chess/m/v2/engine"
@@ -9,14 +11,23 @@ import (
 
 const maxDepth = 5
 const divide = true
+const fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 func main() {
-	b := engine.NewBoard()
-	g := engine.NewGame(&b)
+	b, err := engine.NewBoardFromFEN(strings.NewReader(fen))
+	if err != nil {
+		fatal(err)
+	}
+	g := engine.NewGame(b)
 	start := time.Now()
 	n := perft(g, maxDepth, divide)
 	elapsed := time.Since(start)
 	fmt.Printf("%d nodes, %dms\n", n, elapsed.Milliseconds())
+}
+
+func fatal(v interface{}) {
+	fmt.Println(v)
+	os.Exit(1)
 }
 
 func perft(g engine.Game, depth uint8, divide bool) uint64 {
