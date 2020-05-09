f34a9c5ff4670b73964d514a987ca5cfc3cd0f53
George Bills <george.s.bills@gmail.com>
2020-05-09 22:27:39 +1000

    Move popMSB, popLSB to util.

--- engine/generate.go
+++ engine/generate.go
@@ -662,21 +662,3 @@ func addCaptures(moves []Move, from uint8, movesqs uint64) []Move {
 	}
 	return moves
 }
-
-// popLSB finds the Least Significant Bit in x, returning the index of that bit,
-// a bitmask with only that bit set, and mutating x to unset that bit.
-func popLSB(x *uint64) (uint8, uint64) {
-	idx := uint8(bits.TrailingZeros64(*x))
-	var bit uint64 = 1 << idx
-	*x &^= bit
-	return idx, bit
-}
-
-// popMSB finds the Most Significant Bit in x, returning the index of that bit,
-// a bitmask with only that bit set, and mutating x to unset that bit.
-func popMSB(x *uint64) (uint8, uint64) {
-	idx := uint8(63 - bits.LeadingZeros64(*x))
-	var bit uint64 = 1 << idx
-	*x &^= bit
-	return idx, bit
-}
