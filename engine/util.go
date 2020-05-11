1ac9cc33baffbab1ef20c2cb0e9dd1a6a96eeb22
George Bills <george.s.bills@gmail.com>
2020-05-11 23:16:41 +1000

    Move "internal" doc for PrintOrderedIndex out of the header.

--- engine/util.go
+++ engine/util.go
@@ -91,37 +91,36 @@ func Square(rank, file uint8) uint8 {
 // bottom (printed last) and the 8th rank at the top (printed first). This is
 // also required when printing and parsing FEN, where the 8th rank is again
 // rendered first.
-//
-// The floor of i divided by 8 (number of files) gets us the current rank, in
-// the range 0...7 (i.e. indexed by 0).
-//
-// 7 (max rank if we're indexing by 0) minus the current rank gets us ranks in
-// reverse order (0 ⇨ 7, 1 ⇨ 6, ..., 7 ⇨ 0).
-//
-// i modulus 8 (number of ranks) gets us the current file, again in the range
-// 0...7.
-//
-// The corresponding "reversed rank" index for an input index is then the
-// reverse rank times 8 (ranks) plus the current file.
-//
-// The full formula is thus:
-//
-//       8×( 7 -    ⌊i÷8⌋) + (i mod 8)
-//     = 8×( 7 -    ⌊i÷8⌋) + i - 8×⌊i÷8⌋
-//     =    56 -  8×⌊i÷8⌋  + i - 8×⌊i÷8⌋
-//     =    56 - 16×⌊i÷8⌋  + i
-//
-// This gets us indexes (for input 0...63) in the following order:
-//
-//     56, 57, 58, 59, 60, 61, 62, 63, (rank 8)
-//     48, 49, 50, ...,            55,      (7)
-//     40, 41, 42, ...,            47,      (6)
-//     32, 33, 34, ...,            39,      (5)
-//     24, 25, 26, ...,            31,      (4)
-//     16, 17, 18, ...,            23,      (3)
-//      8,  9, 10, ...,            15,      (2)
-//      0,  1,  2, ...,             6.      (1)
 func PrintOrderedIndex(i uint8) uint8 {
+	// The floor of i divided by 8 (number of files) gets us the current rank,
+	// in the range 0...7 (i.e. indexed by 0).
+	//
+	// 7 (max rank if we're indexing by 0) minus the current rank gets us ranks
+	// in reverse order (0 ⇨ 7, 1 ⇨ 6, ..., 7 ⇨ 0).
+	//
+	// i modulus 8 (number of ranks) gets us the current file, again in the
+	// range 0...7.
+	//
+	// The corresponding "reversed rank" index for an input index is then the
+	// reverse rank times 8 (ranks) plus the current file.
+	//
+	// The full formula is thus:
+	//
+	//       8×( 7 -    ⌊i÷8⌋) + (i mod 8)
+	//     = 8×( 7 -    ⌊i÷8⌋) + i - 8×⌊i÷8⌋
+	//     =    56 -  8×⌊i÷8⌋  + i - 8×⌊i÷8⌋
+	//     =    56 - 16×⌊i÷8⌋  + i
+	//
+	// This gets us indexes (for input 0...63) in the following order:
+	//
+	//     56, 57, 58, 59, 60, 61, 62, 63, (rank 8)
+	//     48, 49, 50, ...,            55,      (7)
+	//     40, 41, 42, ...,            47,      (6)
+	//     32, 33, 34, ...,            39,      (5)
+	//     24, 25, 26, ...,            31,      (4)
+	//     16, 17, 18, ...,            23,      (3)
+	//      8,  9, 10, ...,            15,      (2)
+	//      0,  1,  2, ...,             6.      (1)
 	return i + 56 - 16*(i/8)
 }
 
