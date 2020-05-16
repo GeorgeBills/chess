29f0f3794b3fe94a907adc06557db7284df9790d
George Bills <george.s.bills@gmail.com>
2020-05-16 21:18:26 +1000

    Use io.pipe to combine tests into subtests.

--- uci/uci_test.go
+++ uci/uci_test.go
@@ -2,9 +2,12 @@ package uci_test
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/GeorgeBills/chess/m/v2/uci"
 	"github.com/GeorgeBills/chess/m/v2/uci/mocks"
@@ -24,19 +27,87 @@ func TestQuitBeforeUCI(t *testing.T) {
 	assert.Equal(t, "", w.String())
 }
 
-func TestUCIOK(t *testing.T) {
-	const in = "uci\nquit"
-	r := strings.NewReader(in)
+func TestUCI(t *testing.T) {
+	piper, pipew := io.Pipe()
+
+	var calledNewGame bool
+	var calledIsReady bool
+	var calledSetStartingPosition bool
+	var calledGoDepthWith uint8
 	h := &mocks.Handler{
 		IdentifyFunc: func() (name, author string, other map[string]string) {
 			return Name, Author, nil
 		},
+		NewGameFunc: func() {
+			calledNewGame = true
+		},
+		IsReadyFunc: func() {
+			calledIsReady = true
+		},
+		SetStartingPositionFunc: func() {
+			calledSetStartingPosition = true
+		},
+		GoDepthFunc: func(depth uint8) string {
+			calledGoDepthWith = depth
+			return "a1h8"
+		},
 	}
-	w := &bytes.Buffer{}
-	p := uci.NewParser(h, r, w, ioutil.Discard)
+
+	buf := &bytes.Buffer{}
+
+	go func() {
+		t.Run("uci", func(t *testing.T) {
+			buf.Reset()
+			pipew.Write([]byte("uci\n"))
+			time.Sleep(1 * time.Millisecond) // GROSS... need to be sure parser has done the work
+			assert.Equal(t, "id name test-engine\nid author George Bills\nuciok\n", buf.String())
+		})
+
+		t.Run("isready", func(t *testing.T) {
+			buf.Reset()
+			pipew.Write([]byte("isready\n"))
+			time.Sleep(1 * time.Millisecond) // GROSS... need to be sure parser has done the work
+			assert.Equal(t, "readyok\n", buf.String())
+			assert.True(t, calledIsReady)
+		})
+
+		t.Run("ucinewgame", func(t *testing.T) {
+			buf.Reset()
+			pipew.Write([]byte("ucinewgame\n"))
+			time.Sleep(1 * time.Millisecond) // GROSS... need to be sure parser has done the work
+			assert.Equal(t, "", buf.String())
+			assert.True(t, calledNewGame)
+		})
+
+		t.Run("position startpos", func(t *testing.T) {
+			buf.Reset()
+			pipew.Write([]byte("position startpos\n"))
+			time.Sleep(1 * time.Millisecond) // GROSS... need to be sure parser has done the work
+			assert.Equal(t, "", buf.String())
+			assert.True(t, calledSetStartingPosition)
+		})
+
+		t.Run("go depth", func(t *testing.T) {
+			buf.Reset()
+			pipew.Write([]byte("go depth 123\n"))
+			time.Sleep(1 * time.Millisecond) // GROSS... need to be sure parser has done the work
+			assert.Equal(t, "bestmove a1h8\n", buf.String())
+			assert.EqualValues(t, 123, calledGoDepthWith)
+		})
+
+		t.Run("quit", func(t *testing.T) {
+			buf.Reset()
+			pipew.Write([]byte("quit\n"))
+			time.Sleep(1 * time.Millisecond) // GROSS... need to be sure parser has done the work
+			assert.Equal(t, "", buf.String())
+			// TODO: assert we called some cleanup routine
+		})
+
+		pipew.Close()
+	}()
+
+	p := uci.NewParser(h, piper, buf, os.Stderr)
 	p.Run()
-	const expected = "id name test-engine\nid author George Bills\nuciok\n"
-	assert.Equal(t, expected, w.String())
 }
 
 func TestExtraInformation(t *testing.T) {
@@ -55,100 +126,4 @@ func TestExtraInformation(t *testing.T) {
 	p.Run()
 	const expected = "id name super-chess\nid author Jane Smith\nid release-date 2020-05-16\nid version v1.2.3\nuciok\n"
 	assert.Equal(t, expected, w.String())
-}
-
-func TestNewGame(t *testing.T) {
-	const in = "uci\nucinewgame\nquit"
-	r := strings.NewReader(in)
-	var calledNewGame bool
-	h := &mocks.Handler{
-		IdentifyFunc: func() (name, author string, other map[string]string) {
-			return Name, Author, nil
-		},
-		NewGameFunc: func() {
-			calledNewGame = true
-		},
-	}
-	w := &bytes.Buffer{}
-	p := uci.NewParser(h, r, w, ioutil.Discard)
-	p.Run()
-	const expected = "id name test-engine\nid author George Bills\nuciok\n"
-	assert.Equal(t, expected, w.String())
-	assert.True(t, calledNewGame)
-}
-
-func TestPositionStart(t *testing.T) {
-	const in = "uci\nucinewgame\nposition startpos\nquit"
-	r := strings.NewReader(in)
-	var calledNewGame bool
-	var calledSetStartingPosition bool
-	h := &mocks.Handler{
-		IdentifyFunc: func() (name, author string, other map[string]string) {
-			return Name, Author, nil
-		},
-		NewGameFunc: func() {
-			calledNewGame = true
-		},
-		SetStartingPositionFunc: func() {
-			calledSetStartingPosition = true
-		},
-	}
-	w := &bytes.Buffer{}
-	p := uci.NewParser(h, r, w, ioutil.Discard)
-	p.Run()
-	const expected = "id name test-engine\nid author George Bills\nuciok\n"
-	assert.Equal(t, expected, w.String())
-	assert.True(t, calledNewGame)
-	assert.True(t, calledSetStartingPosition)
-}
-
-func TestIsReady(t *testing.T) {
-	const in = "uci\nisready\nquit"
-	r := strings.NewReader(in)
-	var calledIsReady bool
-	h := &mocks.Handler{
-		IdentifyFunc: func() (name, author string, other map[string]string) {
-			return Name, Author, nil
-		},
-		IsReadyFunc: func() {
-			calledIsReady = true
-		},
-	}
-	w := &bytes.Buffer{}
-	p := uci.NewParser(h, r, w, ioutil.Discard)
-	p.Run()
-	const expected = "id name test-engine\nid author George Bills\nuciok\nreadyok\n"
-	assert.Equal(t, expected, w.String())
-	assert.True(t, calledIsReady)
-}
-
-func TestGoDepth(t *testing.T) {
-	const in = "uci\nucinewgame\nposition startpos\ngo depth 123\nquit"
-	r := strings.NewReader(in)
-	var calledNewGame bool
-	var calledSetStartingPosition bool
-	var calledGoDepthWith uint8
-	h := &mocks.Handler{
-		IdentifyFunc: func() (name, author string, other map[string]string) {
-			return Name, Author, nil
-		},
-		NewGameFunc: func() {
-			calledNewGame = true
-		},
-		SetStartingPositionFunc: func() {
-			calledSetStartingPosition = true
-		},
-		GoDepthFunc: func(depth uint8) string {
-			calledGoDepthWith = depth
-			return "a1h8"
-		},
-	}
-	w := &bytes.Buffer{}
-	p := uci.NewParser(h, r, w, ioutil.Discard)
-	p.Run()
-	const expected = "id name test-engine\nid author George Bills\nuciok\nbestmove a1h8\n"
-	assert.Equal(t, expected, w.String())
-	assert.True(t, calledNewGame)
-	assert.True(t, calledSetStartingPosition)
-	assert.EqualValues(t, 123, calledGoDepthWith)
 }
