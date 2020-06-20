96f9914cdc099415ba5e0b2a672e2e1f85897456
George Bills <george.s.bills@gmail.com>
2020-06-20 11:53:29 +0000

    TestHandleEvents().

---
+++ lichess/eventhandler_test.go
@@ -0,0 +1,43 @@
+package lichess_test
+
+import (
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/GeorgeBills/chess/lichess"
+	"github.com/GeorgeBills/chess/lichess/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleEvents(t *testing.T) {
+	h := &mocks.EventHandlerMock{
+		ChallengeFunc: func(e *lichess.EventChallenge) {},
+		GameStartFunc: func(e *lichess.EventGameStart) {},
+	}
+	eventch := make(chan interface{}, 10)
+	go lichess.HandleEvents(h, log.New(os.Stdout, "", 0), eventch)
+
+	t.Run("challenge", func(t *testing.T) {
+		e := &lichess.EventChallenge{}
+		eventch <- e
+		time.Sleep(10 * time.Millisecond)
+		calls := h.ChallengeCalls()
+		if assert.Len(t, calls, 1) {
+			assert.Same(t, e, calls[0].E)
+		}
+	})
+
+	t.Run("game start", func(t *testing.T) {
+		e := &lichess.EventGameStart{}
+		eventch <- e
+		time.Sleep(10 * time.Millisecond)
+		calls := h.GameStartCalls()
+		if assert.Len(t, calls, 1) {
+			assert.Same(t, e, calls[0].E)
+		}
+	})
+
+	close(eventch)
+}
